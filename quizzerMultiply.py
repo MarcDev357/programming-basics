e628af296dd1f0bbfb06525a9b9c08c7aaf6db7c
MarcDev357 <890005234@ciat.edu>
2024-03-15 13:06:34 -0700

    Create quizzerMultiply.py

---
+++ quizzerMultiply.py
@@ -0,0 +1,27 @@
+# Assignment part 2
+# Import random
+import random
+# Created a main function 
+def main():
+    success = Quizzer()
+
+# Generate random number for problem, ask user question and see if correct. 
+def Quizzer():
+    #Create 2 randint variables to use in problem
+    x = random.randint(1, 10)
+    y = random.randint(1, 10)
+    # Display random multiplication problem
+    print(f"What is {x} x {y}?")
+    #Calculate for the product
+    product = x * y
+    #Store user answer
+    result = int(input("Answer is: "))
+    # if/else to see if results are true/false.
+    if result == product:
+        print("Good Job!")
+        return True
+    else:
+        print("Better luck next time!")
+        return False
+# Call function to run    
+main()
