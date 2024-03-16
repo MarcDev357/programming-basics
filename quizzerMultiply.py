0e1181e4fc4a2fefcc9754f442f2ee590ba5f85b
MarcDev357 <890005234@ciat.edu>
2024-03-16 11:43:56 -0700

    Update quizzerMultiply.py

--- quizzerMultiply.py
+++ quizzerMultiply.py
@@ -13,16 +13,11 @@ def Quizzer():
     print(f"What is {x} x {y}?")
     #Calculate for the product
     product = x * y
-<<<<<<< HEAD
     #Store user answer
     try:
         result = int(input("Answer is: "))
     except ValueError:
         print("Please enter numbers.")
-=======
-    # Store user answer
-    result = int(input("Answer is: "))
->>>>>>> cb84b37bd0b4d20d945aa91cf76e48d2b7b287c0
     # if/else to see if results are true/false.
     if result == product:
         print("Good Job!")
